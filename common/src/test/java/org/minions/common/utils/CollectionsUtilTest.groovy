a29480b3e9cd8f14bd0205527c7678ed87c89e93
korov9 <korov9@163.com>
2021-03-13 04:17:34 +0800

    spock

--- common/src/test/java/org/minions/common/utils/CollectionsUtilTest.groovy
+++ common/src/test/java/org/minions/common/utils/CollectionsUtilTest.groovy
@@ -6,6 +6,11 @@ import spock.lang.Specification
  * @author zhu.lei* @date 2021-03-13 03:33
  */
 class CollectionsUtilTest extends Specification {
+
+    def setup() {
+        println("setup")
+    }
+
     def "test isEmpty"() {
         given:
         def emptyList = new ArrayList();
@@ -26,5 +31,67 @@ class CollectionsUtilTest extends Specification {
         null      | true
         []        | true
         [1, 2, 3] | false
+    }
+
+    def "maximum of two numbers"() {
+        expect:
+        Math.max(a as int, b as int) == c
+
+        where:
+        a << [3, 5, 9]
+        b << [7, 4, 9]
+        c << [7, 5, 9]
+    }
+
+    def "maximum of two numbers1"() {
+        def value = 1
+
+        when:
+        value == 1
+
+        then:
+        println("first")
+
+        then:
+        println("second")
+    }
+
+    def cleanup() {
+        println 'clean up'
+    }
+}
+
+
+abstract class BaseSpec extends Specification {
+    def x = { println 'base field initializer' }()
+
+    def setupSpec() { println 'base setupSpec()' }
+
+    def cleanupSpec() { println 'base cleanupSpec()' }
+
+    def setup() { println 'base setup()' }
+
+    def cleanup() { println 'base cleanup()' }
+
+    def baseSpecMethod() {
+        setup:
+        println 'base spec method'
+    }
+}
+
+class DerivedSpec extends BaseSpec {
+    def y = { println 'derived field initializer' }()
+
+    def setupSpec() { println 'derived setupSpec()' }
+
+    def cleanupSpec() { println 'derived cleanupSpec()' }
+
+    def setup() { println 'derived setup()' }
+
+    def cleanup() { println 'derived cleanup()' }
+
+    def derivedSpecMethod() {
+        setup:
+        println 'derived spec method'
     }
 }
