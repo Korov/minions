424c87750d6ae2c89f1dc2770701245c5ee6cc78
korov9 <korov9@163.com>
2021-03-13 03:32:33 +0800

    优化代码

--- common/src/main/java/org/minions/common/utils/CollectionsUtil.java
+++ common/src/main/java/org/minions/common/utils/CollectionsUtil.java
@@ -4,9 +4,6 @@ import java.util.Collection;
 
 public class CollectionsUtil {
     public static boolean isEmpty(Collection collections) {
-        if (null == collections || 0 == collections.size()) {
-            return false;
-        }
-        return true;
+        return collections != null && !collections.isEmpty();
     }
 }
