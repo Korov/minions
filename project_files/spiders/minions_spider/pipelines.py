b0df1424ff24d82ec97f5875e2f4c5a165888f28
Korov <korov9@163.com>
2021-08-24 00:02:13 +0800

    add mongo pipeline

--- project_files/spiders/minions_spider/pipelines.py
+++ project_files/spiders/minions_spider/pipelines.py
@@ -6,16 +6,25 @@
 
 # useful for handling different item types with a single interface
 import json
+from pymongo import MongoClient
 
 
 class biquge_pipeline(object):
     def open_spider(self, spider):
         self.f = open('biquge.log', 'w', encoding='utf-8')
+        self.client = MongoClient('mongodb://admin:mongo@linux.korov.org:27017')
 
     def process_item(self, item, spider):
         self.f.write(json.dumps(dict(item), ensure_ascii=False))
         self.f.write('\n')
+        db = self.client['spider']
+        collection = db['book_info']
+        book_info = {"book_url": item['book_url'],
+                     "book_name": item['book_name'],
+                     "author_name": item['author_name']}
+        collection.insert_one(book_info)
         return item
 
     def close_spider(self, spider):
         self.f.close()
+        self.client.close()
