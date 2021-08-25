f27e92a4b4921e31093669820f72fe0a9b7cde22
Korov <korov9@163.com>
2021-08-25 13:23:03 +0800

    slow spider

--- project_files/spiders/minions_spider/settings.py
+++ project_files/spiders/minions_spider/settings.py
@@ -28,6 +28,8 @@ ROBOTSTXT_OBEY = False
 # See https://docs.scrapy.org/en/latest/topics/settings.html#download-delay
 # See also autothrottle settings and docs
 # DOWNLOAD_DELAY = 3
+DOWNLOAD_TIMEOUT = 120
+RETRY_ENABLED = False
 # The download delay setting will honor only one of:
 # CONCURRENT_REQUESTS_PER_DOMAIN = 16
 # CONCURRENT_REQUESTS_PER_IP = 16
--- project_files/spiders/minions_spider/spiders/biquge.py
+++ project_files/spiders/minions_spider/spiders/biquge.py
@@ -8,10 +8,19 @@ import scrapy
 from minions_spider.items import biquge_item
 
 headers = {
-    "Accept-Encoding": "gzip, deflate, br",
+    "Host": "www.xbiquge.la",
+    "Connection": "keep-alive",
+    "sec-ch-ua": '" Not A;Brand";v="99", "Chromium";v="92"',
+    "sec-ch-ua-mobile": "?0",
+    "Upgrade-Insecure-Requests": "Upgrade-Insecure-Requests",
     "User-Agent": "Mozilla/5.0 (Windows NT 6.3; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/37.0.2049.0 Safari/537.36",
+    "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9",
+    "Sec-Fetch-Site": "same-origin",
+    "Sec-Fetch-Mode": "navigate",
+    "Sec-Fetch-User": "?1",
+    "Sec-Fetch-Dest": "document",
+    "Accept-Encoding": "gzip, deflate, br",
     "accept-language": "zh-CN,zh;q=0.9,en;q=0.8",
-    "sec-ch-ua": "\" Not A;Brand\";v=\"99\", \"Chromium\";v=\"92\""
 }
 
 
@@ -33,6 +42,7 @@ class biquge(scrapy.Spider):
         books = response.selector.xpath("//span[@class='s2']/a")
         for book in books:
             book_url = str(book.attrib['href'])
+            time.sleep(5)
             yield scrapy.Request(url=book_url, headers=headers, callback=self.parse_chapters)
 
     def parse_chapters(self, response, **kwargs):
@@ -50,8 +60,24 @@ class biquge(scrapy.Spider):
                                     book_author=book_author, book_url=book_url, chapter_url=chapter_url,
                                     chapter_name=chapter_name)
             # 每隔20到30秒爬取下一章节内容
-            time.sleep(random.randint(20, 30))
-            yield scrapy.Request(url=chapter_url, headers=headers, meta={"item": copy.deepcopy(book_item)},
+            time.sleep(random.randint(15, 25))
+            chapter_headers = {
+                "Host": "www.xbiquge.la",
+                "Connection": "keep-alive",
+                "sec-ch-ua": '" Not A;Brand";v="99", "Chromium";v="92"',
+                "sec-ch-ua-mobile": "?0",
+                "Upgrade-Insecure-Requests": "Upgrade-Insecure-Requests",
+                "User-Agent": "Mozilla/5.0 (Windows NT 6.3; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/37.0.2049.0 Safari/537.36",
+                "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9",
+                "Sec-Fetch-Site": "same-origin",
+                "Sec-Fetch-Mode": "navigate",
+                "Sec-Fetch-User": "?1",
+                "Sec-Fetch-Dest": "document",
+                "Accept-Encoding": "gzip, deflate, br",
+                "accept-language": "zh-CN,zh;q=0.9,en;q=0.8",
+                "Referer": book_url
+            }
+            yield scrapy.Request(url=chapter_url, headers=chapter_headers, meta={"item": copy.deepcopy(book_item)},
                                  callback=self.parse_chapter)
 
     def parse_chapter(self, response, **kwargs):
