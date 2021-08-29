51677bf629b9092a74c39a6a85c381d894e3cf8f
Korov <korov9@163.com>
2021-08-29 14:59:37 +0800

    add redis

--- project_files/spiders/debug.py
+++ project_files/spiders/debug.py
@@ -3,4 +3,28 @@ from scrapy import cmdline
 # 在Run/Debug Configurations中添加python的debug，设置Script path为当前项目的begin.py，保存之后点击debug按钮就可以debug了
 # 也可以在这里直接debug
 # scrapy crawl <spider_name>
-cmdline.execute('scrapy crawl biquge'.split())
+# cmdline.execute('scrapy crawl biquge'.split())
+
+import redis
+
+r = redis.Redis(host='korov.myqnapcloud.cn', port=6379, db=0)
+result = r.sadd('set_test', 'bar1', 'bar2')
+print(result)
+values = r.smembers('set_test')
+print(values)
+for value in values:
+    print(value.decode())
+
+temp = 'bar2'
+if temp.encode() in values:
+    print("in")
+else:
+    print("not in")
+
+result = r.sismember('set_test','bar1')
+print(result)
+
+result = r.srem('set_test', 'bar1')
+print(result)
+result = r.sismember('set_test','bar1')
+print(result)
--- project_files/spiders/minions_spider/filters.py
+++ project_files/spiders/minions_spider/filters.py
@@ -2,6 +2,7 @@ import logging
 from scrapy.http import Request
 from scrapy.dupefilters import BaseDupeFilter
 from pymongo import MongoClient
+import redis
 
 
 class BiqugeFilter(BaseDupeFilter):
@@ -9,14 +10,23 @@ class BiqugeFilter(BaseDupeFilter):
         self.client = MongoClient('mongodb://spider:spider@korov.myqnapcloud.cn:27017/spider')
         db = self.client['spider']
         self.collection = db['book_info']
+        self.redis_db = redis.Redis(host='korov.myqnapcloud.cn', port=6379, db=0)
         self.logger = logging.getLogger(__name__)
 
     def request_seen(self, request: Request):
-        old_book_info = {"chapter_url": request.url}
+        old_chapter_url = request.url
+        # redis中只存储正在处理中的url，若存在则说明此url正在被别的爬虫爬取中
+        is_members = self.redis_db.sismember('seen_urls', old_chapter_url)
+        if is_members:
+            return True
+
+
+        old_book_info = {"chapter_url": old_chapter_url}
         count = self.collection.count_documents(old_book_info)
         if count > 0:
             return True
         else:
+            self.redis_db.set("seen_urls", request.url)
             return False
 
     # can return a deferred
