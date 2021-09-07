55aeb3fcba7ca9520f60f303604a203edcd1efc2
Korov <korov9@163.com>
2021-09-08 00:53:39 +0800

    python demo

--- project_files/spiders/demo.py
+++ project_files/spiders/demo.py
@@ -2,7 +2,7 @@ from pymongo import MongoClient
 from pymongo.errors import DuplicateKeyError
 
 url = 'https://www.xbiquge.la/0/82/8234452.html'
-if not url.endswith('html') :
+if not url.endswith('html'):
     print("not html")
 else:
     print("html")
@@ -14,21 +14,8 @@ if 'fenlei' in url:
 client = MongoClient('mongodb://spider:spider@korov.myqnapcloud.cn:27017/spider')
 db = client['spider']
 collection = db['seen_urls']
-old_book_info = {"chapter_url": 'chapter_url'}
-count = collection.delete_one(old_book_info)
-print(count)
-try:
-    count = collection.insert_one(old_book_info)
-except DuplicateKeyError:
-    count = 0
-print(count)
-old_info = collection.find_one(old_book_info)
-print(old_info)
-try:
-    count = collection.insert_one(old_book_info)
-except DuplicateKeyError:
-    count = 0
-print(count)
 
-
-
+count = 0
+for url in collection.find({}):
+    count = count + 1
+    print("url:%s, count:%s", url.get("chapter_url"), count)
