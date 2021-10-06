dd17c4dcd84160042596fba97020bc8b2de2513c
Korov <korov9@163.com>
2021-10-06 23:33:03 +0800

    update spider

--- project_files/spiders/minions_spider/pipelines.py
+++ project_files/spiders/minions_spider/pipelines.py
@@ -13,6 +13,46 @@ from pymongo.errors import DuplicateKeyError
 
 from minions_spider import constant
 
+class XuanshuPipeline(object):
+    def open_spider(self, spider):
+        self.mongo_client = MongoClient(constant.MONGO_URL)
+        mongo_db = self.mongo_client['spider']
+        self.mongo_collection = mongo_db['xuanshu_info']
+        self.redis_db0 = redis.Redis(host=constant.REDIS_HOST, port=constant.REDIS_PORT, db=0,
+                                     username=constant.REDIS_USER,
+                                     password=constant.REDIS_PASSWORD)
+        self.logger = logging.getLogger(__name__)
+
+    def process_item(self, item, spider):
+        book_info = {"book_name": item['book_name'],
+                     "book_description": item['book_description'],
+                     "book_category": item['book_category'],
+                     "book_author": item['book_author'],
+                     "book_url": item['book_url'],
+                     "chapter_url": item['chapter_url'],
+                     "chapter_name": item['chapter_name'],
+                     "chapter_content": item['chapter_content'],
+                     "timestamp": item['timestamp'],
+                     "date_time": item['date_time']}
+        chapter_url = item["chapter_url"]
+        old_book_info = {"chapter_url": chapter_url}
+        count = self.mongo_collection.count_documents(old_book_info)
+        if count == 0:
+            try:
+                self.mongo_collection.insert_one(book_info)
+                self.logger.info("insert book:%s, chapter:%s", item['book_name'], item['chapter_name'])
+            except DuplicateKeyError:
+                self.logger.info("insert failed with book:%s, chapter:%s exists", item['book_name'],
+                                 item['chapter_name'])
+        else:
+            self.logger.info("book:%s, chapter:%s exists", item['book_name'], item['chapter_name'])
+
+        self.redis_db0.srem(spider.redis_value_key, chapter_url)
+        self.logger.info(f"remove key:{spider.redis_value_key}, chapter url:{chapter_url}")
+        return item
+
+    def close_spider(self, spider):
+        self.mongo_client.close()
 
 class BiqugePipeline(object):
     def open_spider(self, spider):
--- project_files/spiders/minions_spider/spiders/xuanshuwang.py
+++ project_files/spiders/minions_spider/spiders/xuanshuwang.py
@@ -1,9 +1,12 @@
-import logging
+import copy
+import datetime
+import time
 
 import scrapy
 from pymongo.mongo_client import MongoClient
-
+from loguru import logger
 from minions_spider import constant
+from minions_spider.items import BiqugeItem
 
 headers = {
     "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9",
@@ -11,7 +14,7 @@ headers = {
     "Accept-Language": "zh-CN,zh;q=0.9",
     "Cache-Control": 'max-age=0',
     "Connection": "keep-alive",
-    "Cookie": "obj=1; clickbids=39094",
+    # "Cookie": "obj=1; clickbids=39094",
     "Host": "www.xuanshu.com",
     "sec-ch-ua": '"Chromium";v="94", "Google Chrome";v="94", ";Not A Brand";v="99"',
     "sec-ch-ua-mobile": "?0",
@@ -27,22 +30,17 @@ headers = {
 mongo_client = MongoClient(constant.MONGO_URL)
 mongo_db_spider = mongo_client['spider']
 book_collection = mongo_db_spider['xuanshu_info']
-logger = logging.getLogger(__name__)
 
 
 class xuanshu(scrapy.Spider):
     name = "xuanshu"
     allowed_domains = ['xuanshu.com']
     custom_settings = {
-        # 'DOWNLOADER_MIDDLEWARES': {'minions_spider.middlewares.biquge_middleware': 300},
-        'DOWNLOAD_TIMEOUT': 120,
+        'ITEM_PIPELINES': {'minions_spider.pipelines.XuanshuPipeline': 300},
+        'CONCURRENT_REQUESTS': 16,
         'CONCURRENT_REQUESTS_PER_DOMAIN': 50,
-        # 'CONCURRENT_REQUESTS_PER_IP': 50,
-        'DOWNLOAD_DELAY': 5,
-        'AUTOTHROTTLE_ENABLED': True,
-        'AUTOTHROTTLE_START_DELAY': 5,
-        'AUTOTHROTTLE_MAX_DELAY': 60,
-        'AUTOTHROTTLE_TARGET_CONCURRENCY': 10.0
+        'DOWNLOAD_DELAY': 2,
+        'COOKIES_ENABLED': False
     }
 
     def start_requests(self):
@@ -54,5 +52,43 @@ class xuanshu(scrapy.Spider):
             yield scrapy.Request(url=url, headers=headers, callback=self.parse_books, priority=1)
 
     def parse_books(self, response, **kwargs):
-        books = response.selector.xpath("//div[@class='xupl']/a")
-        logger.info(books)
+        books = response.selector.xpath("//div[@class='xname']/a")
+        # for book in books:
+        #     book_url = f"https://www.xuanshu.com{str(book.attrib['href'])}"
+        #     headers.pop("Referer")
+        #     yield scrapy.Request(url=book_url, headers=headers, callback=self.parse_chapters, priority=2)
+        book_url = f"https://www.xuanshu.com{str(books[0].attrib['href'])}"
+        headers.pop("Referer")
+        yield scrapy.Request(url=book_url, headers=headers, callback=self.parse_chapters, priority=2)
+
+    def parse_chapters(self, response, **kwargs):
+        chapters = response.selector.xpath("//div[@class='pc_list']/ul/li/a")
+        book_id = str.split(response.url, '/')[4]
+        book_category = response.selector.xpath("//dd/a/text()")[1].extract()
+        book_name = response.selector.xpath("//h1/text()").extract()[0]
+        author_name = str.split(response.selector.xpath("//dl/text()")[0].extract(), "：")[1]
+        book_desc = response.selector.xpath("//div[@class='info_des']//text()")[20].extract().strip()
+        for chapter in chapters:
+            chapter_url = f"https://www.xuanshu.com/book/{book_id}/{str(chapter.attrib['href'])}"
+            chapter_name = str(chapter.xpath("text()").extract()[0])
+
+            book_item = BiqugeItem(book_name=book_name, book_description=book_desc, book_category=book_category,
+                                   book_author=author_name, book_url=f"https://www.xuanshu.com/book/{book_id}", chapter_url=chapter_url,
+                                   chapter_name=chapter_name)
+            yield scrapy.Request(url=chapter_url, headers=headers, meta={"item": copy.deepcopy(book_item)},
+                                 callback=self.parse_chapter, priority=3)
+
+
+    def parse_chapter(self, response, **kwargs):
+        chapter_contents = response.selector.xpath("//div[@id='content1']/text()")
+        lines = []
+        for line in chapter_contents:
+            if line.root != '\r':
+                lines.append(line.root.strip())
+        item = response.meta['item']
+        item['chapter_content'] = lines
+        item['timestamp'] = time.time()
+        item['date_time'] = datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S.%f')
+        logger.info(f"book_name:{item['book_name']}, chapter_name:{item['chapter_name']}")
+        yield item
+
