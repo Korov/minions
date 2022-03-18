e5c09069136144a06222953c72563b5ed98f09ee
Korov <korov9@163.com>
2022-03-18 09:06:51 +0800

    去掉fastjson

--- demo/build.gradle.kts
+++ demo/build.gradle.kts
@@ -15,7 +15,6 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-devtools")
     implementation("org.postgresql:postgresql:${rootProject.extra.get("postgresVersion")}")
     implementation("org.flywaydb:flyway-core:${rootProject.extra.get("flywayVersion")}")
-    implementation("com.alibaba:fastjson:${rootProject.extra.get("fastjsonVersion")}")
     implementation("io.springfox:springfox-boot-starter:${rootProject.extra.get("springfoxVersion")}")
     implementation("org.mybatis.spring.boot:mybatis-spring-boot-starter:${rootProject.extra.get("mybatisVersion")}")
     implementation("com.baomidou:mybatis-plus-boot-starter:${rootProject.extra.get("mybatisPlusVersion")}")
