fff7f4ebc1ca7158f0168dea99e24a8eb00cedb3
Korov <korov9@163.com>
2022-03-22 22:27:31 +0800

    增加redis订阅

--- project_files/spiders/test/util/RedisUtilTest.py
+++ project_files/spiders/test/util/RedisUtilTest.py
@@ -13,16 +13,23 @@ redis_server = redis.StrictRedis(host='localhost', port=6379, db=0, password="re
 # logger.info(redis_server.get(name="demo_key").decode("utf-8"))
 # for i in range(1,100):
 #     logger.info(redis_server.set(name=i, value=i, nx=True))
-logger.info(redis_server.keys(pattern="*"))
-for key in redis_server.scan_iter(match="*"):
-    logger.info(f"key:{key.decode('utf-8')}, value:{redis_server.get(key).decode('utf-8')}")
-logger.info(redis_server.scan(cursor=0, match="*"))
+# logger.info(redis_server.keys(pattern="*"))
+# for key in redis_server.scan_iter(match="*"):
+#     logger.info(f"key:{key.decode('utf-8')}, value:{redis_server.get(key).decode('utf-8')}")
+# logger.info(redis_server.scan(cursor=0, match="*"))
 
 # 实现事务
-with redis_server.pipeline() as pipe:
-    # 监视一个key，如果在执行期间被修改了，抛出WatchError
-    pipe.watch("key_1")
-    pipe.multi()
-    pipe.set("key_1", "value1")
-    pipe.set("key_2", "value2")
-    pipe.execute()
+# with redis_server.pipeline() as pipe:
+#     # 监视一个key，如果在执行期间被修改了，抛出WatchError
+#     pipe.watch("key_1")
+#     pipe.multi()
+#     pipe.set("key_1", "value1")
+#     pipe.set("key_2", "value2")
+#     pipe.execute()
+
+redis_server.publish(channel="channel1", message="message1")
+
+redis_subscribe = redis_server.pubsub()
+redis_subscribe.subscribe("channel1")
+for item in redis_subscribe.listen():
+    logger.info(item)
