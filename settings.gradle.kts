b7f23a26e6222258c8758d8603ea5b7a95bcfe3a
Korov <korov9@163.com>
2022-05-09 00:35:43 +0800

    增加gateway

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -2,4 +2,8 @@ rootProject.name = "minions"
 include("demo")
 include("common")
 include("kafka-consumer")
+<<<<<<< Updated upstream
 include("gateway")
+=======
+include("minions_gateway")
+>>>>>>> Stashed changes
