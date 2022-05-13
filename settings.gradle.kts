4f43bd4949e04a1ea423b643207c167caa72c36e
Korov <korov9@163.com>
2022-05-14 00:12:27 +0800

    解决冲突

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -2,8 +2,4 @@ rootProject.name = "minions"
 include("demo")
 include("common")
 include("kafka-consumer")
-<<<<<<< Updated upstream
 include("gateway")
-=======
-include("minions_gateway")
->>>>>>> Stashed changes
