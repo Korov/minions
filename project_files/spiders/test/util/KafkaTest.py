6a000e4d48744ecaa4a8ac012f51d80d11e94392
Korov <korov9@163.com>
2022-08-24 16:01:46 +0800

    update kafka test

--- project_files/spiders/test/util/KafkaTest.py
+++ project_files/spiders/test/util/KafkaTest.py
@@ -1,9 +1,7 @@
-from kafka import KafkaAdminClient
-from kafka.admin import ConfigResourceType, ConfigResource
 from loguru import logger
 
-from minions_spider.util import kafka_consumer as kafka_consumer, kafka_producer as kafka_producer, \
-    kafka_admin as kafka_admin
+from minions_spider.util import kafka_consumer as kConsumer, kafka_producer as kProducer, \
+    kafka_admin as kAdmin
 
 logger.add('test.log', rotation="100 MB",
            format="{time:YYYY-MM-DD HH:mm:ss.SSS} - {thread.name} - {file} - {level} - "
@@ -11,19 +9,18 @@ logger.add('test.log', rotation="100 MB",
            level="INFO")
 
 # servers = "localhost:9092"
-servers = "192.168.205.135:30092"
-consumer = kafka_consumer.create_consumer(bootstrap_servers=servers, group_id="monitor")
+servers = "192.168.1.19:9092"
+consumer = kConsumer.create_consumer(bootstrap_servers=servers, group_id="monitor")
 
-kafka_consumer.consumer_msg(topic="flink_siem", bootstrap_servers=servers, group_id="monitor",
-                            auto_offset_reset="latest")
-# kafka_consumer.display_topic_consumers_count(consumer, ["flink_siem"])
-# kafka_consumer.display_authorized_topics(kafka_consumer=consumer)
-# kafka_consumer.consumer_seek(consumer, "flink_siem", 4, 16573117)
-# kafka_producer.send_msg(bootstrap_servers=servers, topic="alert_box", key="box_threat_data", msg="test")
+kConsumer.consumer_msg(topic="log_river", bootstrap_servers=servers, group_id="monitor", auto_offset_reset="latest")
+# kConsumer.display_topic_consumers_count(consumer, ["log_river"])
+# kConsumer.display_authorized_topics(kafka_consumer=consumer)
+# kConsumer.consumer_seek(consumer, "flink_siem", 4, 16573117)
+# kProducer.send_msg(bootstrap_servers=servers, topic="alert_box", key="box_threat_data", msg="test")
+#
+# kAdmin.list_topics(bootstrap_servers=servers)
+# kAdmin.describe_topics(bootstrap_servers=servers, topics=['sink_topic'])
+# kAdmin.list_consumer_groups(bootstrap_servers=servers)
+# kAdmin.list_consumer_group_offsets(bootstrap_servers=servers, group_ids=["monitor"])
 
-# kafka_admin.list_topics(bootstrap_servers=servers)
-# kafka_admin.describe_topics(bootstrap_servers=servers, topics=[])
-# kafka_admin.list_consumer_groups(bootstrap_servers=servers)
-# kafka_admin.list_consumer_group_offsets(bootstrap_servers=servers, group_ids=["monitor"])
-
-# kafka_admin.describe_configs(bootstrap_servers=servers)
+# kAdmin.describe_configs(bootstrap_servers=servers)
