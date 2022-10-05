270f8e2db37daa9ede88cdf84b74c927dc4caf1a
Korov <korov9@163.com>
2022-10-05 12:41:16 +0800

    update project

--- project_files/spiders/test/util/KafkaTest.py
+++ project_files/spiders/test/util/KafkaTest.py
@@ -9,18 +9,18 @@ logger.add('test.log', rotation="100 MB",
            level="INFO")
 
 # servers = "localhost:9092"
-servers = "192.168.1.19:9092"
+servers = "korov.myqnapcloud.cn:9092"
 consumer = kConsumer.create_consumer(bootstrap_servers=servers, group_id="monitor")
 
-kConsumer.consumer_msg(topic="log_river", bootstrap_servers=servers, group_id="monitor", auto_offset_reset="latest")
-# kConsumer.display_topic_consumers_count(consumer, ["log_river"])
+# kConsumer.consumer_msg(topic="log_river_topic", bootstrap_servers=servers, group_id="monitor", auto_offset_reset="latest")
+# kConsumer.display_topic_consumers_count(consumer, ["log_river_topic", "alert_topic"])
 # kConsumer.display_authorized_topics(kafka_consumer=consumer)
 # kConsumer.consumer_seek(consumer, "flink_siem", 4, 16573117)
 # kProducer.send_msg(bootstrap_servers=servers, topic="alert_box", key="box_threat_data", msg="test")
 #
 # kAdmin.list_topics(bootstrap_servers=servers)
-# kAdmin.describe_topics(bootstrap_servers=servers, topics=['sink_topic'])
+# kAdmin.describe_topics(bootstrap_servers=servers, topics=['alert_topic'])
 # kAdmin.list_consumer_groups(bootstrap_servers=servers)
 # kAdmin.list_consumer_group_offsets(bootstrap_servers=servers, group_ids=["monitor"])
 
-# kAdmin.describe_configs(bootstrap_servers=servers)
+kAdmin.describe_configs(bootstrap_servers=servers)
